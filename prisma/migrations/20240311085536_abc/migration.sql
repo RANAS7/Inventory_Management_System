517fc2f94c5e0513d14c863efe038beb14120a80
crowninnovation <crowninnovation2022@gmail.com>
2024-03-18 22:45:14 +0545

    fix: migration

---
+++ prisma/migrations/20240311085536_abc/migration.sql
@@ -0,0 +1,134 @@
+-- CreateTable
+CREATE TABLE `user` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NULL,
+    `Email` VARCHAR(150) NULL,
+    `Role` VARCHAR(100) NULL,
+    `Password` VARCHAR(100) NULL,
+
+    UNIQUE INDEX `Email`(`Email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `salary` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `Date` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `Amount` DECIMAL(10, 2) NULL,
+    `User_ID` INTEGER NULL,
+
+    INDEX `User_ID`(`User_ID`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `supplier_id` INTEGER NULL,
+    `Image` TEXT NULL,
+    `product` VARCHAR(150) NULL,
+    `quantity` INTEGER NULL,
+    `price` DECIMAL(10, 2) NULL,
+    `total` DECIMAL(10, 2) NULL,
+
+    INDEX `supplier_id`(`supplier_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `daily_exp` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `created_by` INTEGER NULL,
+    `amount` DECIMAL(10, 2) NULL,
+    `payment_type` VARCHAR(50) NULL,
+    `detail` TEXT NULL,
+
+    INDEX `created_by`(`created_by`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `miscellaneous` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `created_by` INTEGER NULL,
+    `amount` DECIMAL(10, 2) NULL,
+    `payment_type` VARCHAR(50) NULL,
+    `detail` TEXT NULL,
+
+    INDEX `created_by`(`created_by`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `notice` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(200) NULL,
+    `message` TEXT NULL,
+    `user_id` INTEGER NULL,
+
+    INDEX `user_id`(`user_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `vendor` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NULL,
+    `email` VARCHAR(150) NULL,
+    `contact` VARCHAR(40) NULL,
+    `contact_person` VARCHAR(100) NULL,
+    `address` VARCHAR(100) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `customer` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(150) NULL,
+    `email` VARCHAR(200) NULL,
+    `contact` VARCHAR(50) NULL,
+    `address` VARCHAR(100) NULL,
+    `contact_person` VARCHAR(100) NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `sales` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `date` DATETIME(0) NULL DEFAULT CURRENT_TIMESTAMP(0),
+    `customer_id` INTEGER NULL,
+    `product_id` INTEGER NULL,
+    `quantity` INTEGER NULL,
+    `price` DECIMAL(10, 2) NULL,
+    `total` DECIMAL(10, 2) NULL,
+    `payment_type` VARCHAR(100) NULL,
+
+    INDEX `customer_id`(`customer_id`),
+    INDEX `product_id`(`product_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `salary` ADD CONSTRAINT `salary_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_ibfk_1` FOREIGN KEY (`supplier_id`) REFERENCES `vendor`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `daily_exp` ADD CONSTRAINT `daily_exp_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `miscellaneous` ADD CONSTRAINT `miscellaneous_ibfk_1` FOREIGN KEY (`created_by`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `notice` ADD CONSTRAINT `notice_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `user`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `sales` ADD CONSTRAINT `sales_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customer`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+-- AddForeignKey
+ALTER TABLE `sales` ADD CONSTRAINT `sales_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
